dbc91e75e9ee6b7fddf836ecb2bd5e84ec7d8f88
JoseAle123 <jmachacamu@unsa.edu.pe>
2024-12-22 20:42:24 -0500

    Refactorizando UserProfileCard.js #6

--- frontend/birdie/src/components/Profile/UserProfileCard.js
+++ frontend/birdie/src/components/Profile/UserProfileCard.js
@@ -2,15 +2,17 @@ import { Avatar } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import useUserContext from "../../contexts/UserContext";
+import PropTypes from "prop-types";  // Correcta importación
 
 const UserProfileCard = (props) => {
     const { id, profile_pic, username, followers } = props;
     const {
         profileData: { id: userId },
     } = useUserContext();
+
     return (
         <div className="mt-2 pl-3 p-2 bg-gray-50 w-full flex gap-2 items-center rounded-lg dark:bg-[#030108]">
-            <Avatar src={profile_pic ? profile_pic : null} alt={username}>
+            <Avatar src={profile_pic || null} alt={username}>
                 {username.at(0).toUpperCase()}
             </Avatar>
             <div className="flex flex-col mr-auto">
@@ -27,6 +29,14 @@ const UserProfileCard = (props) => {
             </Link>
         </div>
     );
+};
+
+// Validación de las props usando PropTypes
+UserProfileCard.propTypes = {
+    id: PropTypes.string.isRequired,        // 'id' debe ser un string y es requerido
+    profile_pic: PropTypes.string,          // 'profile_pic' debe ser un string, pero es opcional
+    username: PropTypes.string.isRequired,  // 'username' debe ser un string y es requerido
+    followers: PropTypes.number.isRequired, // 'followers' debe ser un número y es requerido
 };
 
 export default UserProfileCard;
